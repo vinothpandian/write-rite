8ec339649b6ff8ff587b5a12472626416e09b4d3
Vinoth Pandian <pandian@fit.fraunhofer.de>
2019-06-03 17:08:03 +0200

    progressing: db update

--- src/containers/DashboardPage/index.jsx
+++ src/containers/DashboardPage/index.jsx
@@ -1,29 +1,43 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
+
 import { compose } from 'recompose';
 import { withTheme } from '../../contexts/Theme';
 import SignOut from '../../components/SignOut';
 import customProps from '../../proptypes';
 import { withAuthorization } from '../../contexts/Session';
+import Firebase, { withFirebase } from '../../contexts/Firebase';
 
-const DashboardPage = ({ theme, toggleTheme }) => (
-  <div
-    className={theme.className}
-    style={{
-      background: theme.backgroundColor,
-      color: theme.fontColor,
-    }}
-  >
-    Dashboard
-    <div>
-      <SignOut />
-      <button type="button" onClick={toggleTheme}>
-        Toggle
-      </button>
+const DashboardPage = ({ firebase, theme, toggleTheme }) => {
+  const addWriting = () => {
+    const key = firebase.createWriting();
+    console.log(key);
+  };
+
+  return (
+    <div
+      className={theme.className}
+      style={{
+        background: theme.backgroundColor,
+        color: theme.fontColor,
+      }}
+    >
+      Dashboard
+      <div>
+        <SignOut />
+        <button type="button" onClick={toggleTheme}>
+          Toggle
+        </button>
+        <button type="button" onClick={addWriting}>
+          Add Writing
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 DashboardPage.propTypes = {
+  firebase: PropTypes.instanceOf(Firebase).isRequired,
   theme: customProps.theme.isRequired,
   toggleTheme: customProps.toggleTheme.isRequired,
 };
@@ -32,5 +46,6 @@ const condition = user => !!user;
 
 export default compose(
   withTheme,
+  withFirebase,
   withAuthorization(condition),
 )(DashboardPage);
--- src/contexts/Firebase/firebase.js
+++ src/contexts/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 import firebaseConfig from './config';
 
@@ -7,6 +8,10 @@ class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
     this.auth = app.auth();
+    this.databaseRef = ref => app
+      .database()
+      .ref(`users/${ref}`)
+      .child('writings');
   }
 
   createUser({ email, password }) {
@@ -20,6 +25,23 @@ class Firebase {
   signOutUser() {
     this.auth.signOut();
   }
+
+  createWriting(userId) {
+    return this.databaseRef(`${userId}`).push().key;
+  }
+
+  // async getTitles(userId) {
+  //   const value = await this.app
+  //     .database()
+  //     .ref(`users/${userId}`)
+  //     .once('writings');
+
+  //   return Object.keys(value);
+  // }
+
+  // async updateWriting(userId, postKey, writing) {
+
+  // }
 }
 
 export default Firebase;
